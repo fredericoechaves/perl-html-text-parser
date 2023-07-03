c0a96264ae641020146d53f798601c17b32816ae
Frederico Chaves <frederico.chaves@gmail.com>
2023-07-03 19:52:50 -0300

    tentativas de wget

--- call_me.sh
+++ call_me.sh
@@ -1,1 +1,2 @@
-cat index.html | ./extract_text.pl article img em span | ./remove_blank_lines.pl
+cat como-configurar-a-justificativa-de-horas-extras.html | ./extract_text.pl article img em span | ./remove_blank_lines.pl
+
