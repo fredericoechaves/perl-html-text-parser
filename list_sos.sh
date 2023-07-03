6ad7e2a702b6ba84b25fd1f77f7c7c68a6355a67
Frederico Chaves <frederico.chaves@gmail.com>
2023-07-03 19:26:55 -0300

    funcionando o perl para extrair o texto do link apenas

--- list_sos.sh
+++ list_sos.sh
@@ -1,1 +1,1 @@
-cat sos.xml | grep -i '<item>' -A 5 | grep -i '<link>' | grep -iv '/?' | perl -nle'print $1 if m{(link>\K.*(?=</link>))}'
+cat sos.xml | grep -i '<item>' -A 5 | grep -i '<link>' | grep -iv '/?' | perl -nle'print $1 if m{link>(.*)(?=</link>)}'
